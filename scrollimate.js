c8de5c47ccbcb6ce138cef2a216720773da3f558
Moritz <moritz@mailmmc.com>
2018-03-05 10:11:01 -0600

    minor formatting updates

--- scrollimate.js
+++ scrollimate.js
@@ -7,7 +7,7 @@
  * https://creativecommons.org/licenses/by/4.0/legalcode
  *
  */
-var scrollimate = (function( window, $ ){
+var scrollimate = (function( $ ){
 
   var _global = {
     wp:   0,
@@ -742,4 +742,4 @@ var scrollimate = (function( window, $ ){
     nonIDTabs: nonIDTabs,
     enableMobile: enableMobile,
   };
-})(window, jQuery);
+})(jQuery);
